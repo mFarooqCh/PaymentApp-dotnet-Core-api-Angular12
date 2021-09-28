b3d81f615841378348bd014a38a5febd1de46835
mFarooqCh <farooqchaudhry749@gmail.com>
2021-09-28 16:21:35 +0500

    Initial commit

---
+++ PaymentAPI/PaymentAPI/Models/PaymentDetailContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentAPI.Models
+{
+    //this class will be used to create the physicalcopies of the class that asre define here in dataset
+    public class PaymentDetailContext : DbContext
+    {
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options): base(options) //we initialize the base class constructor with this constructor
+        {
+                
+        }
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
